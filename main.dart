c7eba3fc6a50003ad5485753025829465e97c0c9
zhushuihua <zhushuihua@yahoo.com>
2026-04-16 14:47:28 +0800

    update

--- main.dart
+++ main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() async {}
+void main() async {
+  print('OK');
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Text('abc');
   }
 }
