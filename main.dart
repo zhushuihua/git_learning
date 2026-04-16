6ada9094eed7fd709e3a8364de073572c27bb941
zhushuihua <zhushuihua@yahoo.com>
2026-04-16 14:44:46 +0800

    add widget

--- main.dart
+++ main.dart
@@ -1,3 +1,12 @@
-void main() {
-  print('done');
+import 'package:flutter/material.dart';
+
+void main() async {}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
